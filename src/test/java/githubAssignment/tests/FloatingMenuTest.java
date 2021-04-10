bda3eee28c68a17ee564d42a62fa512449a9d607
Marwan Subhi <marwan.subhi4@gmail.com>
2021-04-09 23:25:22 -0400

    More stuff

--- src/test/java/githubAssignment/tests/FloatingMenuTest.java
+++ src/test/java/githubAssignment/tests/FloatingMenuTest.java
@@ -12,7 +12,7 @@ public class FloatingMenuTest {
     FloatingMenu fl = new FloatingMenu();
 
     @Test
-    public void test(){
+    public void test() {
 
         //Floating Menu
         //
@@ -23,11 +23,11 @@ public class FloatingMenuTest {
 
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
-      //  js.executeScript("arguments[0].scrollIntoView(true);",fl.bottomLink);
+        //  js.executeScript("arguments[0].scrollIntoView(true);",fl.bottomLink);
         Assert.assertTrue(fl.floatingBar.isDisplayed());
 
 
-        for (int i =1; i<7; i++){
+        for (int i = 1; i < 7; i++) {
 
             BrowserUtils.wait(1);
             js.executeScript("window.scrollBy(0, 750)");
@@ -35,12 +35,37 @@ public class FloatingMenuTest {
             Assert.assertTrue(fl.floatingBar.isDisplayed());
         }
 
-        for (int i =1; i<7; i++){
+        for (int i = 1; i < 7; i++) {
 
             BrowserUtils.wait(1);
             js.executeScript("window.scrollBy(0, -750)");
 
             Assert.assertTrue(fl.floatingBar.isDisplayed());
+        }
+
+    }
+
+    @Test
+    public void test2() {
+
+        int[] arr = {0,2,3,5,0,1,4,2,3,0}; // 1, 2, -----  3,4 -----5,6----- 7,8
+
+        int target = 5;
+
+//        for (int i =0, j =0 ; i<arr.length && j<arr.length ; i++, j++){
+//
+//            if (arr[i]+arr[j]==5) System.out.println(i + "  "+j);
+//
+//
+//        }
+
+        //0,2,3,5,0,1,4,2,3,0
+        for (int i =0 ; i<arr.length  ; i++){
+
+            for (int j = 1; j<arr.length ; j++){
+
+                if (arr[i]+arr[j]==target) System.out.println(i + "  "+j);
+            }
         }
 
 
